6d20c932c25e653a69bc923af97a3d73e397f95e
Pranav <pranav.techiegeek@gmail.com>
2023-11-20 20:59:54 +0530

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import React from "react";
+
 export default function App() {
-	return <div className="App"></div>;
+	const counter = 0;
+
+	return (
+		<div className="App">
+			<h1>
+				Counter: <span>{counter}</span>
+			</h1>
+			<button>Increment</button>
+			<button>Decrement</button>
+		</div>
+	);
 }
