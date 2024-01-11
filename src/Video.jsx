f8da2d79298d676cedb266d3d598621decfcf122
Pranav <pranav.techiegeek@gmail.com>
2024-01-11 13:11:45 +0530

    ready

--- src/Video.jsx
+++ src/Video.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-export default function Video() {
-	// write your code here
-	return null
+export default function Video(props) {
+	const { url } = props
+
+	return (
+		<div className="video">
+			<video className="player" loop src={url}></video>
+		</div>
+	)
 }
